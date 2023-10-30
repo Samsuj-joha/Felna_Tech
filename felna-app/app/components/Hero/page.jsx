804f9b229e7d46157457a9fefc0b0f438617e920
Samsuj-joha <samsujjoha99@gmail.com>
2023-10-30 17:48:35 +0600

    included hero-section all changrs file

--- felna-app/app/components/Hero/page.jsx
+++ felna-app/app/components/Hero/page.jsx
@@ -1,18 +1,79 @@
+"use client"
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
-function Hero() {
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Image from 'next/image'
+import Slider1 from './../../../Images/slider-1.jpg'
+import Slider2 from './../../../Images/slide-2.jpg'
+import Slider3 from './../../../Images/slide-3.jpg'
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const Hero= () => {
   return (
-    <>
-    <div className="container mx-auto flex">
-        <div className="hero-left basis-1/2">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam earum reiciendis deserunt non consequatur suscipit voluptatum, debitis aperiam tempora voluptate quam harum est cupiditate? Adipisci labore a corrupti expedita natus aut, iure voluptas quo alias ullam assumenda explicabo nam, nulla quidem. Earum vitae perspiciatis incidunt! Reprehenderit accusamus facilis doloribus odio!</p>
+   <>
+    <div className=" py-8 gap-5 2xl:flex 2xl:container 2xl:mx-auto   ">
+      <div className=' mx-auto  '>
+        <Swiper className='lg:max-w-screen-lg lg:h-3/4 xl:max-w-screen-lg xl:h-3/4 2xl:max-w-screen-lg 2xl:h-3/4 sm:mx-auto md:mx-auto lg:mx-auto  xl:mx-auto'
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide>
+            <Image className='object-cover'
+              src={Slider1}
+              width={1100}
+              height={500}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image className='object-cover'
+              src={Slider1}
+              width={1100}
+              height={500}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image className='object-cover'
+              src={Slider1}
+              width={1100}
+              height={500}
+            />
+          </SwiperSlide>
+          
+        </Swiper>
+      </div>
+      <div className='grid-cols-2 '>
+        <div className='flex flex-col justify-center items-center gap-5 py-3'>
+          <div className="right-1">
+            <Image className='object-cover '
+                src={Slider2}
+                width={400}
+                height={100}
+              />
+          </div>
+          <div className="right-2">
+            <Image className='object-cover'
+                src={Slider3}
+                width={400}
+                height={400}
+            />
+          </div>
         </div>
-        <div className="hero-right basis-96">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat delectus fugit reprehenderit rem libero alias officia voluptate nihil veritatis excepturi?</p>
-        </div>
+      </div>
     </div>
-       
-    </>
-  )
-}
-
+      
+   </>
+  );
+};
 export default Hero
